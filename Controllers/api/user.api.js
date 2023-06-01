8125994775ca340703548ae32ba477aa5f64281c
DuyNewbie <nbduy4606@gmail.com>
2023-06-01 21:45:11 +0700

    Duy

--- Controllers/api/user.api.js
+++ Controllers/api/user.api.js
@@ -14,9 +14,21 @@ exports.list = async (req , res , next) => {
 
 exports.loginApp = async (req , res , next) => {
 
+    let userName = req.query.UserName;
+    let passWord = req.quiry.PassWord;
+
     let checkLogin = false;
 
-    
+    try{
+        let objUser = await mdUser.userModel.find({username : userName});
+
+        if(objUser.passWord == passWord){
+            checkLogin = true;
+        }
+
+    }catch(err){
+        console.log(err);
+    }
 
     res.status(200).json(
         {
