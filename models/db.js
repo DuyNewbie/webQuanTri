007b366da76840f658462c2a0881e85c954c3b27
DuyNewbie <nbduy4606@gmail.com>
2023-08-01 11:48:38 +0700

    Update db.js

--- models/db.js
+++ models/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://nbduy4606:w20ZPN7I3sbs0MCl@quanlyduan.rvi6loa.mongodb.net/GoCoffee?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://nbduy4606:3kqpG5NZalDHkvZF@quanlyduan.rvi6loa.mongodb.net/GoCoffee?retryWrites=true&w=majority')
         .catch((err) =>{
             console.log("Loi ket noi CSDL");
             console.log(err);
